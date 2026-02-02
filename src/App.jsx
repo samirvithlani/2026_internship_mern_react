ca4a8dd6ca8e4b2ab91a59f9f65018cf3774ba13
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-02 09:51:23 +0530

    mapdemo

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import viteLogo from "/vite.svg";
 import { HeaderComponent } from "./components/HeaderComponent";
 import { FooterComponent } from "./components/FooterComponent";
 import ContentComponent from "./components/ContentComponent";
+import { MapDemo1 } from "./components/MapDemo1";
+import { MapDemo2 } from "./components/MapDemo2";
+import { MapDemo3 } from "./components/MapDemo3";
+import { MapDemo4 } from "./components/MapDemo4";
+import { MapDemo5 } from "./components/MapDemo5";
 
 function App() {
 
@@ -12,7 +17,7 @@ function App() {
   return (
     <div>
       <HeaderComponent></HeaderComponent>
-      <ContentComponent></ContentComponent>
+      <MapDemo5></MapDemo5>
       <FooterComponent></FooterComponent>
     </div>
   );
